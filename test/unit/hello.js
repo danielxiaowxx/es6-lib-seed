219a80d698288da10c66e86d629d5c152e155364
danielxiaowxx <danielwxx@gmail.com>
2016-10-05 22:46:02 +0800

    add

--- test/unit/hello.js
+++ test/unit/hello.js
@@ -18,12 +18,12 @@ describe('hello', () => {
       expect(hello.sayHi).to.have.returned('Hello daniel');
     });
 
-    it('Provate a invalid type name parameter', () => {
+    it('Provide a invalid type name parameter', () => {
       try {
         hello.sayHi(1);
       } catch(e) {}
       expect(hello.sayHi).to.have.thrown('Error'); // 这里是错误的类型，比如new Error()就是Error，new TypeError()就是TypeError
-    })
+    });
     // 尽量覆盖所有入参测试用例 - 参数无值情况，参数有效情况，参数无效情况
 
   });
